9308966b83ade00762dcbb1b386fd35791e7ac0a
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-08-10 23:05:19 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Country from './pages/Country'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
 function App() {
-  return <h1>App</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country" element={<Country />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
