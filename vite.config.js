0ce562b792f86915832162021ba4b443a0af4599
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-09-04 21:33:09 +0530

    added base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: 'https://vsompura3.github.io/rest-countries-api-with-color-theme-switcher-master/',
+  plugins: [react()],
 })
